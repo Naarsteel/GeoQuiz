c9db60132b34ba8be788cdaf7acc36d43c88f3b6
Naarsteel <re.nation@yandex.ru>
2025-06-19 18:10:28 +0700

    Включение команды регистрации сообщения в onCreate(Bundle?) (MainActivity.kt)

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geomain
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
