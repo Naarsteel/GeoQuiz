fc63e507356833551ad551d1b98093d8f87d6816
Naarsteel <re.nation@yandex.ru>
2025-06-19 17:45:19 +0700

    Инкапсуляция с помощью функции (MainActivity.kt)

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -50,10 +50,13 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            questionTextView.setText(questionTextResId)
+            updateQuestion()
         }
 
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
