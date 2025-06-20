a75117bb73fa22a51e77332af300939909f5c07f
Naarsteel <re.nation@yandex.ru>
2025-06-20 08:35:54 +0700

    Функция newIntent(...) для CheatActivity (CheatActivity.kt)

--- app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
@@ -1,10 +1,10 @@
 package com.bignerdranch.android.geomain
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.bignerdranch.android.geoquiz.answer_is_true"
@@ -14,10 +14,13 @@ class CheatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cheat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
         }
     }
 }
