439476736c2ca89cf180707bfaaeca5532af0825
Naarsteel <re.nation@yandex.ru>
2025-06-20 08:39:36 +0700

    Добавление функциональности подсматривания ответов (CheatActivity.kt)

--- app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
@@ -3,14 +3,18 @@ package com.bignerdranch.android.geomain
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 private const val EXTRA_ANSWER_IS_TRUE =
-    "com.bignerdranch.android.geoquiz.answer_is_true"
+    "com.bignerdranch.android.geomain.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
 
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
     private var answerIsTrue = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +23,15 @@ class CheatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cheat)
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
     }
 
     companion object {
